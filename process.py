876edfbdfa28227fa42776fa813dd3c2366adb9d
Thialfihar <thi@thialfihar.org>
2016-01-16 22:06:31 +0100

    Commit all changes
    Guess I stopped that in the process of analysing the scene and which
    characters are in it and such.
    
    I moved to Python3, added a requirements.txt, tox.ini for flake8.

--- process.py
+++ process.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Process a parsed novel.
 
@@ -7,42 +7,100 @@ import argparse
 import nltk
 import pickle
 from collections import defaultdict
-from pprint import pprint
+from functools import reduce
+
+from structure import Character, Entity, Scene
+from data import Data
 
 
-def update_dict(dict1, dict2):
-    for key, value in dict2.items():
-        dict1[key] |= value
+def collect(tree, entities=None, words=None):
+    """
+    Collect all entities in a tree.
 
+    """
+    if entities is None:
+        entities = defaultdict(int)
+    if words is None:
+        words = defaultdict(int)
 
-def collect_entities(tree):
-    entities = defaultdict(set)
     if tree.node != 'S':
-        entities[tree.node].add(tuple(tree.leaves()))
+        entity = Entity.get(tree)
+        if entity.name not in ('Ser', 'Lord'):
+            entities[entity] += 1
+    elif not words:
+        for word in tree.leaves():
+            words[word[0].lower()] += 1
 
     subtrees = list(tree.subtrees())[1:]
     for subtree in subtrees:
-        update_dict(entities, collect_entities(subtree))
+        collect(subtree, entities, words)
 
-    return entities
+    return entities, words
+
+
+def marker(text):
+    sentences = reduce(lambda a, b: a + b, [[]] + text[:3])
+    return ' '.join([x[0] for x in sentences])[:32]
 
 
 def process(input_file):
+    """
+    Go through a parsed novel. Chapter by chapter.
+
+    """
     novel = pickle.load(open(input_file))
-    all_entities = defaultdict(set)
+    scene = Scene()
+    data = Data()
     for chapter, paragraphs in novel:
         for text, speech, speech_info in paragraphs:
+            current_speaker = None
+            for sentence in speech_info:
+                tree = nltk.chunk.ne_chunk(sentence)
+                entities, words = collect(tree)
+                data.add(entities, words)
+                if not current_speaker:
+                    for entity in entities:
+                        if isinstance(entity, Character):
+                            current_speaker = entity
+                            break
+
             for sentence in text:
                 tree = nltk.chunk.ne_chunk(sentence)
-                entities = collect_entities(tree)
-                update_dict(all_entities, entities)
+                entities, words = collect(tree)
+                data.add(entities, words)
+                if not current_speaker:
+                    for entity in entities:
+                        if isinstance(entity, Character):
+                            current_speaker = entity
+                            break
 
-    for key, value in all_entities.items():
-        print key
-        for x in value:
-            print '    ', ' '.join([y[0] for y in x])
+            if speech:
+                guessed = False
+                if not current_speaker:
+                    current_speaker = scene.get_speaker(-2)
+                    guessed = True
+                if not current_speaker:
+                    print('not sure who is speaking', speech)
+                else:
+                    print(current_speaker,
+                          'is speaking %s' %
+                          (' (guessed) %s' % str(marker(speech)) if guessed else ''))
 
-        print
+                scene.add_speaker(current_speaker)
+            else:
+                print('nobody is speaking')
+
+            for sentence in speech:
+                tree = nltk.chunk.ne_chunk(sentence)
+                entities, words = collect(tree)
+                data.add(entities, words, character=current_speaker)
+
+        scene = Scene()
+
+    top_words = sorted(data.overall.words.items(), key=lambda x: x[1],
+                       reverse=True)[:100]
+    for word, count in top_words:
+        print(word, count)
 
 
 if __name__ == '__main__':
