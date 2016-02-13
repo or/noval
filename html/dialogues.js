44cc92582f11dde9d11d38593e2237806966b2df
Oliver Runge <oliver.runge@gmail.com>
2016-02-13 15:48:37 +0100

    Improve d3 graph loads
    Edge values and accumulated edge values for a node now adjust the forces
    applied to them, resulting in a much more realistic graph.
    
    One also can adjust the filtering of minor edges with a slider, as well
    as the default charge and link distance to play with the network a bit.
    
    Lastly one can choose to only include linked nodes when edges have been
    filtered.

--- html/dialogues.js
+++ html/dialogues.js
@@ -1,18 +1,60 @@
 var Network = function() {
   this.load = function(selector, data) {
+    var network = this;
     this.dom_node = $(selector);
     this.width = this.dom_node.width() - 10;
     this.height = this.dom_node.height() - 10;
 
-    var network = this;
+    this.cutoff_slider = d3.slider()
+      .min(0.01)
+      .max(1)
+      .value(0.5)
+      .orientation("vertical")
+      .on("slide", function(evt, value) {
+        this.update();
+      }.bind(this));
+
+    d3.select("#cutoff-slider").call(this.cutoff_slider);
+
+    this.charge_slider = d3.slider()
+      .min(-10)
+      .max(-500)
+      .value(-70)
+      .on("slide", function(evt, value) {
+        this.force_update();
+      }.bind(this));
+
+    d3.select("#charge-slider").call(this.charge_slider);
+
+    this.link_distance_slider = d3.slider()
+      .min(10)
+      .max(80)
+      .value(30)
+      .on("slide", function(evt, value) {
+        this.force_update();
+      }.bind(this));
+
+    d3.select("#node-distance-slider").call(this.link_distance_slider);
+
+    this.linked_nodes_only = d3.select("#linked-nodes-only");
+    this.linked_nodes_only.on("change", function(value) {
+      this.update();
+    }.bind(this));
 
     function zoom() {
       if (d3.event.defaultPrevented) return;
-      network.graph.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+      this.graph.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
     }
+
+    this.node_double_click = function(d) {
+      d3.event.stopPropagation();
+      d3.select(this).classed("fixed", d.fixed = false);
+      network.force.start();
+    }
+
     var zoom = d3.behavior.zoom()
       .scaleExtent([1, 8])
-      .on("zoom", zoom);
+      .on("zoom", zoom.bind(this));
 
     this.svg = d3.select(selector).append("svg")
       .attr("width", this.width)
@@ -59,31 +101,41 @@ var Network = function() {
       .style("fill", "none")
       .style("pointer-events", "all");
 
-    this.linksG = this.graph.attr("id", "links");
-    this.nodesG = this.graph.attr("id", "nodes");
+    this.force_update = function() {
+      this.force
+        .size([this.width, this.height])
+        .gravity(0.1)
+        .charge(function(d) {
+          var value = d.edge_value_sum || 0;
+          var v = value / network.max_edge_value_sum;
+          v = Math.sqrt(v);
+          v = Math.sqrt(v);
+          return network.charge_slider.value() * (1 + v);
+        })
+        .linkStrength(function(d) {
+          return 0.5 + 1 * d.normalized_value;
+        })
+        .linkDistance(function(d) {
+          return network.link_distance_slider.value() * (1.5 - d.normalized_value);
+        })
+
+      this.force.start();
+    }
 
     this.force = d3.layout.force()
-      .size([this.width, this.height])
-      .charge(-100)
-      .linkDistance(50)
-      .linkStrength(1.5)
       .on("tick", function() {
-        network.link.attr("d", function(d) {
+        this.link.attr("d", function(d) {
           return "M" + d.source.x + "," + d.source.y
             + "S" + d.intermediate.x + "," + d.intermediate.y
             + " " + d.target.x + "," + d.target.y;
         });
 
-        network.node
+        this.node
           .attr("cx", function(d) { return d.x; })
           .attr("cy", function(d) { return d.y; });
-      });
+      }.bind(this));
 
-    this.node_double_click = function(d) {
-      d3.event.stopPropagation();
-      d3.select(this).classed("fixed", d.fixed = false);
-      network.force.start();
-    }
+    this.force_update();
 
     function dragstart(d) {
       d3.event.sourceEvent.stopPropagation();
@@ -97,29 +149,23 @@ var Network = function() {
     this.update();
   }
 
-  this.build_node_map = function(nodes) {
-    this.node_map = {};
-    var network = this;
-    nodes.forEach(function (n) {
-      network.node_map[n.id] = n;
-    });
-  }
-
   this.load_data = function(data) {
-    var network = this;
-
     this.data = data;
-    this.data.nodes.forEach(function(n) {
-      n.x = Math.floor(network.width / 2 + Math.random() * 100);
-      n.y = Math.floor(network.height / 2 + Math.random() * 100);
-      n.radius = 10;
+    this.all_nodes = this.data.nodes.slice();
+    this.all_edges = this.data.edges.slice();
+    this.node_map = {};
+
+    this.all_nodes.forEach(function(n) {
+      n.x = Math.floor(this.width / 2 + Math.random() * 100);
+      n.y = Math.floor(this.height / 2 + Math.random() * 100);
+      n.radius = 15;
       n.weight = 1;
 
       if (n.image) {
-        n.image_id = "pattern_" + network.pattern_id;
+        n.image_id = "pattern_" + this.pattern_id;
         var size = 100;
-        network.pattern_id += 1;
-        network.defs.append("svg:pattern")
+        this.pattern_id += 1;
+        this.defs.append("svg:pattern")
           .attr("id", n.image_id)
           .attr("x", "0%")
           .attr("y", "0%")
@@ -133,46 +179,77 @@ var Network = function() {
           .attr("x", "0%")
           .attr("y", "0%");
       }
-    });
+      this.node_map[n.id] = n;
 
-    this.data.edges = this.data.edges.filter(function(e) {
-      return e.flag != "cutoff";
-    });
+      n.edge_value_sum = 0;
+    }.bind(this));
 
-    this.build_node_map(this.data.nodes);
-
-    this.edge_map = {}
-    var network = this;
     this.max_edge_value = 0;
-    this.data.edges.forEach(function (e) {
-      e.source = network.node_map[e.from];
-      e.target = network.node_map[e.to];
-      network.max_edge_value = Math.max(network.max_edge_value, e.value);
+    this.all_edges.forEach(function(e) {
+      this.max_edge_value = Math.max(this.max_edge_value, e.value);
+    }.bind(this));
 
-      network.edge_map[e.source.id + "-" + e.target.id] = 1;
-      network.edge_map[e.target.id + "-" + e.source.id] = 1;
-    });
+    this.max_edge_value_sum = 0;
+    this.all_edges.forEach(function(e) {
+      var s = this.node_map[e.from],
+          t = this.node_map[e.to],
+          i = {id: s.id + "_" + t.id,
+               type: "intermediate"}; // intermediate node
+
+      i.x = (s.x + t.x) / 2;
+      i.y = (s.y + t.y) / 2;
+
+      var v = e.value / this.max_edge_value;
+      v = Math.sqrt(v);
+      v = Math.sqrt(v);
+      if (v < 0.01) {
+        v = 0.01;
+      } else if (v > 0.99) {
+        v = 1.0;
+      }
+      e.normalized_value = v;
+
+      this.all_nodes.push(i);
+      e.source = s;
+      e.target = t;
+      e.intermediate = i;
+
+      e.source.edge_value_sum += e.value;
+      e.target.edge_value_sum += e.value;
+
+      this.max_edge_value_sum = Math.max(this.max_edge_value_sum, e.source.edge_value_sum);
+      this.max_edge_value_sum = Math.max(this.max_edge_value_sum, e.target.edge_value_sum);
+    }.bind(this));
   }
 
   this.update = function() {
+    var edges = [];
     var links = [];
-    var nodes = this.data.nodes.slice();
-    this.data.edges.forEach(function(e) {
+    var linked_node_map = {};
+    this.all_edges.forEach(function(e) {
       var s = e.source,
           t = e.target,
-          i = {}; // intermediate node
+          i = e.intermediate;
 
-      i.x = Math.floor(this.width / 2 + Math.random() * 100);
-      i.y = Math.floor(this.height / 2 + Math.random() * 100);
+      if (e.normalized_value < 1 - this.cutoff_slider.value()) {
+        return;
+      }
 
-      nodes.push(i);
-      links.push({source: s, target: i}, {source: i, target: t});
-      e.intermediate = i;
-    });
+      links.push({source: s, target: i, value: e.value, normalized_value: e.normalized_value},
+                 {source: i, target: t, value: e.value, normalized_value: e.normalized_value});
+      edges.push(e);
+      linked_node_map[s.id] = s;
+      linked_node_map[t.id] = t;
+      linked_node_map[i.id] = i;
+    }.bind(this));
+
     this.force.links(links);
 
-    this.link = this.linksG.selectAll("line.link")
-      .data(this.data.edges, function(d) { return d.source.id + "_" + d.target.id; });
+    if (this.link) {
+      this.link.remove();
+    }
+    this.link = this.graph.selectAll("line.link")
+      .data(edges, function(d) { return d.source.id + "_" + d.target.id; });
 
     this.link.enter().append("path")
       .attr("class", "link")
@@ -181,14 +258,26 @@ var Network = function() {
       .attr("stroke-opacity", 0.8)
       .attr("stroke-width", function(d) {
         return d.value * 8 / this.max_edge_value;
-      }.bind(this))
+      }.bind(this));
 
     this.link.exit().remove();
 
+    var nodes;
+    if (this.linked_nodes_only.property("checked")) {
+      nodes = [];
+      for (var key in linked_node_map) {
+        nodes.push(linked_node_map[key]);
+      }
+    } else {
+      nodes = this.all_nodes.slice();
+    }
     this.force.nodes(nodes);
 
-    this.node = this.nodesG.selectAll("circle.node")
-      .data(this.data.nodes, function(d) { return d.id; });
+    if (this.node) {
+      this.node.remove();
+    }
+    this.node = this.graph.selectAll("circle.node")
+      .data(nodes, function(d) { return d.id; });
 
     this.node.enter().append("circle")
       .attr("id", function(d) { return "node_" + d.id; })
@@ -209,6 +298,9 @@ var Network = function() {
       .style("filter", "url(#shadow)")
       .on("dblclick", this.node_double_click)
       .call(this.drag);
+
+    this.node.append("title")
+      .text(function(d) { return d.title; });
 
     //node.on("mouseover", showDetails)
     //  .on("mouseout", hideDetails)
