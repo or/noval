f2e3035d1281d5f82dc6e88c2ced4756b5b49d3c
Oliver Runge <oliver.runge@gmail.com>
2016-02-15 10:22:12 +0100

    Set default completeness to 0.5

--- html/dialogues.js
+++ html/dialogues.js
@@ -8,7 +8,7 @@ var Network = function() {
     this.cutoff_slider = d3.slider()
       .min(0.01)
       .max(1)
-      .value(0.54)
+      .value(0.51)
       .on("slide", function(evt, value) {
         d3.select("#cutoff-slider label .value").text(value.toFixed(2));
         this.update();
