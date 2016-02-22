af85feebdd2b1d3b798b2d2b1f95277b5d35480f
Oliver Runge <oliver.runge@gmail.com>
2016-02-22 21:11:18 +0100

    Resize character images to 200x
    ...easier on the webserver.

--- resize_character_pics.sh
+++ resize_character_pics.sh
@@ -3,5 +3,5 @@
 cd html/images/characters
 IFS=$(echo -en "\n\b")
 for i in $(ls -1 *.jpg); do
-    convert $i -resize 400x -quality 95 small/$i
+    convert $i -resize 200x -quality 95 small/$i
 done
