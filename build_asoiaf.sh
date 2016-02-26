465196885e772ed53b37c90a30326d02decae119
Oliver Runge <oliver.runge@gmail.com>
2016-02-26 21:54:14 +0100

    Add Jaqen H'ghar

--- build_asoiaf.sh
+++ build_asoiaf.sh
@@ -2,6 +2,6 @@
 
 for i in 01-got 02-cok 03-sos 04-ffc 05-dwd all-got; do
   echo "processing $i..."
-  #./process_novel.sh $i.txt.fixed
+  ./process_novel.sh $i.txt.fixed
   ./build_dialogues_json.py --groups html/data/asoiaf/groups.json $i.txt.fixed.read.tagged.entities.normalized.stats html/data/asoiaf/$i.json
 done
